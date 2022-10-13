98fcd3c2c1dff091a76b379e2c03ccbef2d1c514
Fatma <fmohamed34@gmail.com>
2022-10-13 11:36:05 -0400

    Exercise1-complete

---
+++ ObjectsScripts/LoopScripts/ArrayScripts/my_family.js
@@ -0,0 +1,7 @@
+//Part1 Page: 2-16
+let kids = ["Natalie", "Brittany", "Zachary", "Cindy"]
+console.log(kids.length)
+for (let i=0; i<kids.length; i++){
+    console.log(kids[i]);
+}
+
---
+++ ObjectsScripts/LoopScripts/for_loops.js
@@ -0,0 +1,14 @@
+//Part1 is where we start e.g. i=0
+//Part2 where do we stop e.g. 7
+//Part3 how do we get there e.g. i++ AKA increment by one
+//For - Part1; Part2; Part3;
+for(let i=0; i < 7; i++){
+console.log(i, "I LOVE YOU");
+}
+
+//while works similarly but the parts are on separate lines
+let i = 1; //where to start
+while (i <= 7) {   //where to stop
+console.log("I LOVE YOU")
+i++; //increment (go up by one like i=i+1)
+}
